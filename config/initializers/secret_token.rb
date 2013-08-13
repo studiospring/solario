95a020cd4433c005f1b51120b7471593fd77066a
studiospring <sean@studiospring.com.au>
2013-08-13 21:16:43 +1000

    create rails4 site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Solar::Application.config.secret_key_base = '05f631010699c16c9768e141afce8173bc48c89207ee20599e2fc5489c725111b4f14f70b078a23cb673940c59f914a78577f4818ccf5812b955ea18a9b906f1'
