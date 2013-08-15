74dceeec2b542d50670c98f81d1d9bde5042222f
studiospring <sean@studiospring.com.au>
2013-08-15 16:56:46 +1000

    create Postcode mvc files with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Solar::Application.routes.draw do
+  resources :postcodes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
