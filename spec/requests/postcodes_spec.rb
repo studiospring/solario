373608d55fab35ad1894ed96f745a77731a39179
studiospring <sean@studiospring.com.au>
2013-08-16 21:40:05 +1000

    begin postcode tests

--- spec/requests/postcodes_spec.rb
+++ spec/requests/postcodes_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "Postcodes" do
-  describe "GET /postcodes" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get postcodes_path
-      response.status.should be(200)
-    end
+  subject { page }
+
+  shared_examples_for "all postcode pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: title) }
+  end
+  describe 'postcodes page' do
+    let(:heading) { 'Postcodes' }
+    let(:title) { 'Postcodes' }
+    before { visit postcodes_path }
+
+    it_should_behave_like 'all postcode pages'
   end
 end
