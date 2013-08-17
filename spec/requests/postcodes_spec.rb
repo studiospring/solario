7acbe284b661a0172a0085d47bd703189b074d05
studiospring <sean@studiospring.com.au>
2013-08-17 11:46:08 +1000

    initial postcode integration tests green

--- spec/requests/postcodes_spec.rb
+++ spec/requests/postcodes_spec.rb
@@ -1,17 +1,28 @@
 require 'spec_helper'
 
 describe "Postcodes" do
+  let(:base_title) { "Solario" }
+  let(:postcode) { FactoryGirl.create(:postcode) }
   subject { page }
 
   shared_examples_for "all postcode pages" do
     it { should have_selector('h1', text: heading) }
-    it { should have_selector('title', text: title) }
   end
-  describe 'postcodes page' do
+  describe 'index page' do# <<<
     let(:heading) { 'Postcodes' }
-    let(:title) { 'Postcodes' }
     before { visit postcodes_path }
 
     it_should_behave_like 'all postcode pages'
-  end
+    it { should have_title(full_title('Postcodes')) }
+
+    it "should list each postcode" do
+      Postcode.all.each do |postcode|
+        page.should have_selector('td', text: postcode.postcode)
+        page.should have_link('Show', href: postcode_path(postcode))
+        page.should have_link('Delete', href: postcode_path(postcode))
+      end
+    end
+
+    it { should have_link 'Add postcode', href: new_postcode_path }
+  end# >>>
 end
