713326862198038e67965da105b5485ab8b8ba23
studiospring <sean@studiospring.com.au>
2013-08-17 15:59:11 +1000

    postcodes validation tests failing

--- spec/models/postcode_spec.rb
+++ spec/models/postcode_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Postcode do
-  before { @postcode =  Postcode.new(postcode: 4321, suburb: 'Simsville', state: 'ABC', latitude: -12.123123, longitude: 123.456789) }
+  before { @postcode =  Postcode.new(postcode: 4321, suburb: 'Simsville', state: 'WA', latitude: -12.123123, longitude: 123.456789) }
   subject { @postcode }
 
   it { should respond_to(:postcode) }
@@ -14,6 +14,54 @@ describe Postcode do
 
   describe 'when postcode is not present' do
     before { @postcode.postcode = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when postcode is the wrong length' do
+    before { @postcode.postcode = '12345' }
+    it { should_not be_valid }
+  end
+  describe 'when postcode is not a number' do
+    before { @postcode.postcode = 'abcd' }
+    it { should_not be_valid }
+  end
+  
+  describe 'when suburb is not present' do
+    before { @postcode.suburb = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'when state is not present' do
+    before { @postcode.state = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when state is not on list' do
+    before { @postcode.state = 'ABC' }
+    it { should_not be_valid }
+  end
+
+  describe 'when latitude is not present' do
+    before { @postcode.latitude = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when latitude is outside range' do
+    before { @postcode.latitude = '12' }
+    it { should_not be_valid }
+  end
+  describe 'when latitude is too long' do
+    before { @postcode.latitude = '-12.1111111' }
+    it { should_not be_valid }
+  end
+
+  describe 'when longitude is not present' do
+    before { @postcode.longitude = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when longitude is outside range' do
+    before { @postcode.longitude = '12' }
+    it { should_not be_valid }
+  end
+  describe 'when longitude is too long' do
+    before { @postcode.longitude = '-12.1111111' }
     it { should_not be_valid }
   end
 end
