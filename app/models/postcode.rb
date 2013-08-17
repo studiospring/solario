b9e17e27a2a9a8ff35e468a836b4b69dfccc87ca
studiospring <sean@studiospring.com.au>
2013-08-17 18:24:08 +1000

    postcodes validation tests passing

--- app/models/postcode.rb
+++ app/models/postcode.rb
@@ -1,2 +1,17 @@
 class Postcode < ActiveRecord::Base
+  validates :postcode,  presence: true,
+                        length: { is: 4 },
+                        numericality: { only_integer: true }
+  validates :suburb,    presence: true
+  validates :state,     presence: true,
+                        inclusion: { in: %w(ACT NSW NT QLD SA TAS VIC WA) }
+  validates :latitude,  presence: true,
+                        length: { maximum: 10 },
+                        inclusion: -14..10
+                        #numericality: { less_than: 10 },
+                        #numericality: { greater_than: -14 }
+  validates :longitude, presence: true,
+                        length: { maximum: 10 },
+                        numericality: { less_than: 140 },
+                        numericality: { greater_than: 100 }
 end
