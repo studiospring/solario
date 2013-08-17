970ab7b6adde9beaebf1b4e49521de29247cf7c8
studiospring <sean@studiospring.com.au>
2013-08-17 15:40:45 +1000

    begin postcode_spec

--- spec/models/postcode_spec.rb
+++ spec/models/postcode_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Postcode do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @postcode =  Postcode.new(postcode: 4321, suburb: 'Simsville', state: 'ABC', latitude: -12.123123, longitude: 123.456789) }
+  subject { @postcode }
+
+  it { should respond_to(:postcode) }
+  it { should respond_to(:suburb) }
+  it { should respond_to(:state) }
+  it { should respond_to(:latitude) }
+  it { should respond_to(:longitude) }
+
+  it { should be_valid }
+
+  describe 'when postcode is not present' do
+    before { @postcode.postcode = ' ' }
+    it { should_not be_valid }
+  end
 end
