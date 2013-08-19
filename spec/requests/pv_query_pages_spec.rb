a6a8dc857a0037feaa9cda29c57e5f02b86748d5
studiospring <sean@studiospring.com.au>
2013-08-19 15:07:53 +1000

    pv_query index tests failing

--- spec/requests/pv_query_pages_spec.rb
+++ spec/requests/pv_query_pages_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
-describe "PvQueryPages" do
-  describe "GET /pv_query_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get pv_query_pages_index_path
-      response.status.should be(200)
-    end
+describe "PvQuery" do
+  let(:base_title) { "Solario" }
+  let(:pv_query) { FactoryGirl.create(:pv_query) }
+  subject { page }
+
+  shared_examples_for "all pv_query pages" do
+    it { should have_selector('h1', text: heading) }
   end
+  describe 'index page' do# <<<
+    let(:heading) { 'Queries' }
+    before { visit pv_query_path }
+
+    it_should_behave_like 'all pv_query pages'
+    it { should have_title(full_title('Queries')) }
+
+    it "should list each query" do
+      PvQuery.all.each do |query|
+        page.should have_selector('td', text: query.postcode)
+        page.should have_link('Show', href: pv_query_path(query))
+        page.should have_link('Delete', href: pv_query_path(query))
+        expect { click_link 'Delete', href: pv_query_path(query) }.to change(PvQuery, :count).by(-1)
+      end
+    end
+  end# >>>
 end
