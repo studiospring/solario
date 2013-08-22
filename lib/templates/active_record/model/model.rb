c5db4ef61a6baf21e458093c7122012db8665070
studiospring <sean@studiospring.com.au>
2013-08-22 17:14:29 +1000

    work on model template

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -1,9 +1,15 @@
 <% module_namespacing do -%>
-class <%= class_name %> < <%= parent_class_name.classify %>
+  class <%= class_name %> < <%= parent_class_name.classify %>
+    <% attributes.select(&:reference?).each do |attribute| -%>
+      belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
+    <% end -%>
+    <% if attributes.any?(&:password_digest?) -%>
+      has_secure_password
+    <% end -%>
 
-<%- attributes each do |k, v| -%>
-  validates <%- ":#{k}" -%>, presence: true
-<%- end -%>
+    <% attributes each do |name| -%>
+      validates <% ":#{name}" -%>, presence: true
+    <% end -%>
 
-end
+  end
 <% end -%>
