388b407c892eddf4531379e0b26f519285a75f8c
studiospring <sean@studiospring.com.au>
2013-08-22 21:36:42 +1000

    validate panels

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,3 +1,12 @@
 class Panel < ActiveRecord::Base
   belongs_to :pv_query, dependent: :destroy
+
+  validates :tilt,        presence: true,
+                          length: { maximum: 2 },
+                          numericality: { only_integer: true }
+  validates :bearing,     presence: true,
+                          length: { maximum: 3 },
+                          numericality: { only_integer: true }
+  validates :panel_size,  presence: true
+
 end
