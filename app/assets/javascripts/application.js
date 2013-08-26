3be8b1fb30f3feaa20945ba66c97af5fcc5d53e5
studiospring <sean@studiospring.com.au>
2013-08-26 17:01:59 +1000

    organise assets more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-alert('test');
-#creates link to remove form field. Requires accepts_nested_attributes_for association. See railscast #197
-jQuery ->
-  window.remove_fields = (link) -> 
-    $(link).prev("input[type=hidden]").val("1")
-    $(link).closest('.nested_fields').hide()
-
-#add fields. See railscast #197
-  window.add_fields = (link, association, content) ->
-    new_id = new Date().getTime()
-    regexp = new RegExp("new_" + association, "g")
-    $(link).before(content.replace(regexp, new_id))
-    $('input.dob').datepicker({
-      minDate: null,
-      yearRange: 'c-100:c'
-    });
-
