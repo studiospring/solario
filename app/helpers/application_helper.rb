643e7b763e7283d84aa259e406446c653417a4c9
studiospring <sean@studiospring.com.au>
2013-08-26 18:52:50 +1000

    pv_query saves and shows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,6 @@ module ApplicationHelper
     end
   end# >>>
   #see railscast #197
-  #had to replace AR methods and change 'h' to 'raw'
   def link_to_add_fields(name, f, association)# <<<
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
