7835644d4a5ceb4c0c6f8de304a9b3b527b826ad
studiospring <sean@studiospring.com.au>
2013-08-26 17:52:36 +1000

    nested form working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   #see railscast #197
   #had to replace AR methods and change 'h' to 'raw'
   def link_to_add_fields(name, f, association)# <<<
-    new_object = f.object.class.reflect_on_assocation(association).klass.new
+    new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
