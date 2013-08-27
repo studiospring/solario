d80e0e3d98e2b107a810a9b9b5666a353366edf5
studiospring <sean@studiospring.com.au>
2013-08-28 09:39:05 +1000

    panel validation corrected

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -3,14 +3,15 @@ class Panel < ActiveRecord::Base
 
   validates :tilt,        presence: true,
                           length: { maximum: 2 },
-                          numericality: { greater_than_or_equal_to: 0 },
-                          numericality: { less_than_or_equal_to: 90 }
+                          numericality: { greater_than_or_equal_to: 0,
+                                          less_than_or_equal_to: 90 }
   validates :bearing,     presence: true,
                           length: { maximum: 3 },
-                          numericality: { greater_than_or_equal_to: 0 },
-                          numericality: { less_than_or_equal_to: 360 }
+                          numericality: { greater_than_or_equal_to: 0, 
+                                          less_than_or_equal_to: 360 }
   validates :panel_size,  presence: true,
-                          format: { with: /^\d+\.?\d{0,2}$/, multiline: true }
-  #validates :pv_query_id, presence: true
+                          format: { with: /^\d+\.?\d{0,2}$/,
+                                    multiline: true }
+  validates :pv_query_id, presence: true
 
 end
