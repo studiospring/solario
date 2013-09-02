686b4bb3485fd3b4eeb8c1703f4af058e544c63f
studiospring <sean@studiospring.com.au>
2013-09-02 17:34:35 +1000

    remove deprecated be_close...
    replace with be_within().of()

--- spec/models/solar_time_spec.rb
+++ spec/models/solar_time_spec.rb
@@ -14,13 +14,13 @@ describe SolarTime do
     describe 'b' do
       before { @solar_time.b(10) }
       it "should return 'b' degrees for a given day" do
-        @solar_time.b(10).should be_close(-1.222, 0.002)
+        @solar_time.b(10).should be_within(0.002).of(-1.222)
       end
     end
     describe 'EoT' do
       before { solar_time.eot(10) }
       it "should return the Equation of Time for a given day" do
-        @solar_time.eot(10).should be_close(-7.499, 0.002)
+        @solar_time.eot(10).should be_within(0.002).of(-7.499)
       end
     end
     
@@ -29,13 +29,13 @@ describe SolarTime do
     describe 'time correction factor' do
       before { @solar_time.time_correction(10) }
       it "should correct the time wrt day and position" do
-        @solar_time.time_correction(10).should be_close(-67.499, 0.02)
+        @solar_time.time_correction(10).should be_within(0.02).of(-67.499)
       end
     end
     describe 'to_lst' do# <<<
       before { @solar_time.to_lst(14, -67.499) }
       it "should convert local time to local solar time" do
-        @solar_time.to_lst(14, -67.499).should be_close(12.88, 0.01)
+        @solar_time.to_lst(14, -67.499).should be_within(0.01).of(12.88)
       end
     end# >>>
     describe 'hra' do
