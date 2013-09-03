8d7260367e77ea40ada7d35ed56345dc8e166d80
studiospring <sean@studiospring.com.au>
2013-09-03 16:48:05 +1000

    remove unnecessary solar_time tests...
    many methods are called by other methods, so individual tests are unnecessary

--- spec/models/solar_time_spec.rb
+++ spec/models/solar_time_spec.rb
@@ -1,41 +1,15 @@
 require 'spec_helper'
 
 describe SolarTime do
-  let(:sun) { Sun.new(:longitude, :day) }
+  let(:sun) { Sun.new(:latitude, :longitude, :day) }
   #let(:sun) { FactoryGirl.create(:sun) }
-  before { @sun = Sun.new(135, 10) }
+  before { @sun = Sun.new(-20, 135, 10) }
   subject { @sun }
 
-  it "dummy instance has longitude attribute" do
-    @sun.longitude.should == 135
-  end
-
-  describe 'class method' do
-    describe 'b' do
-      before { @sun.b(10) }
-      it "should return 'b' degrees for a given day" do
-        @sun.b(10).should be_within(0.002).of(-1.222)
-      end
-    end
-    describe 'EoT' do
-      before { sun.eot(10) }
-      it "should return the Equation of Time for a given day" do
-        @sun.eot(10).should be_within(0.002).of(-7.499)
-      end
-    end
-    
-  end
-  describe 'instance method' do
-    describe 'time correction factor' do
-      before { @sun.time_correction(10) }
-      it "should correct the time wrt day and position" do
-        @sun.time_correction(10).should be_within(0.02).of(-67.499)
-      end
-    end
     describe 'to_lst' do# <<<
-      before { @sun.to_lst(14, -67.499) }
+      before { @sun.to_lst(14) }
       it "should convert local time to local solar time" do
-        @sun.to_lst(14, -67.499).should be_within(0.01).of(12.88)
+        @sun.to_lst(14).should be_within(0.01).of(12.88)
       end
     end# >>>
     describe 'hra' do
@@ -44,5 +18,4 @@ describe SolarTime do
         @sun.hra(12.88).should be_within(0.01).of(0.23)
       end
     end
-  end
 end
