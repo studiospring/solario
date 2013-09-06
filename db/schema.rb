b36c7de0532c2a114eed06871eed4fc94aae194c
studiospring <sean@studiospring.com.au>
2013-09-06 19:55:23 +1000

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824051631) do
+ActiveRecord::Schema.define(version: 20130906055635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "irradiances", force: true do |t|
+    t.text     "direct"
+    t.text     "diffuse"
+    t.integer  "postcode_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "irradiances", ["postcode_id"], name: "index_irradiances_on_postcode_id", using: :btree
 
   create_table "panels", force: true do |t|
     t.integer "tilt"
