eaf159f4e72e9092c0bacef01d81df912c5f2345
studiospring <sean@studiospring.com.au>
2013-09-07 10:04:21 +1000

    postcode has_one irradiance

--- app/models/irradiance.rb
+++ app/models/irradiance.rb
@@ -1,10 +1,11 @@
 class Irradiance < ActiveRecord::Base
+  belongs_to :postcode
     
-      validates :direct,       presence: true
-      validates :diffuse,       presence: true
-      validates :postcode_id,       presence: true
-                          length: {},
-                        numericality: {},
-                        inclusion: {}
+  validates :direct,       presence: true
+  validates :diffuse,      presence: true
+  validates :postcode_id,  presence: true
+    #length: {},
+    #numericality: {},
+    #inclusion: {}
 
 end
