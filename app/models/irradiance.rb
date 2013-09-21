cfbfd169a8b85720b661fa34dcba2877a9273636
studiospring <sean@studiospring.com.au>
2013-09-21 11:57:20 +1000

    pass more irradiance tests...
    but irradiance still does not create new irradiance

--- app/models/irradiance.rb
+++ app/models/irradiance.rb
@@ -1,9 +1,12 @@
 class Irradiance < ActiveRecord::Base
   belongs_to :postcode
+  #serialize :direct, JSON
+  #serialize :diffuse, JSON
     
   validates :direct,       presence: true
   validates :diffuse,      presence: true
   validates :postcode_id,  presence: true,
-    numericality: { only_integer: true }
+    numericality: { only_integer: true ,
+                    message: 'Postcode_id is not a number' }
 
 end
