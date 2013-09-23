9377cdf55a2a476062029f535e2fe838161821a8
studiospring <sean@studiospring.com.au>
2013-09-24 09:42:42 +1000

    mild refactor of annual_dni_received

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -4,18 +4,23 @@ class String
   #convert '0 1 3 5 ...' to { 1: [0, 1, 3, 5], 2: [...] }
   #annual increment is number of times solar readings are averaged per year
   #daily increment is number of times solar readings are averaged per day
-  def data_string_to_hash(annual_increment, daily_increment)
-    #TODO: error handling, if numbers do not match
+  def data_string_to_hash(annual_increment)
     #convert string to array
     data_array = self.split
-    data_hash = Hash.new
-    annual_increment.times do |count|
-      one_day = Array.new
-      daily_increment.times do 
-        one_day << data_array.shift.to_f
+    daily_increment = data_array.count / annual_increment
+    if daily_increment.is_a? Integer
+      data_hash = Hash.new
+      annual_increment.times do |count|
+        one_day = Array.new
+        daily_increment.times do 
+          one_day << data_array.shift.to_f
+        end
+        data_hash[count] = one_day
       end
-      data_hash[count] = one_day
+      return data_hash
+    else
+      #log error
+      logger.debug 'Number of dni values does not match annual_increment.'
     end
-    return data_hash
   end
 end
