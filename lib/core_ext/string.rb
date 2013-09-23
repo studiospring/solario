4db91324aaf926a7e03f149c515e0a105e04c16c
studiospring <sean@studiospring.com.au>
2013-09-23 15:44:44 +1000

    extend String class with data_string_to_hash...
    to convert irradiance string to hash

---
+++ lib/core_ext/string.rb
@@ -0,0 +1,21 @@
+#To use:
+#require 'core_ext/string'
+class String
+  #convert '0 1 3 5 ...' to { 1: [0, 1, 3, 5], 2: [...] }
+  #annual increment is number of times solar readings are averaged per year
+  #daily increment is number of times solar readings are averaged per day
+  def data_string_to_hash(annual_increment, daily_increment)
+    #TODO: error handling, if numbers do not match
+    #convert string to array
+    data_array = self.split
+    data_hash = Hash.new
+    annual_increment.times do |count|
+      one_day = Array.new
+      daily_increment.times do 
+        one_day << data_array.shift
+      end
+      data_hash[:count] = one_day
+    end
+    return data_hash
+  end
+end
