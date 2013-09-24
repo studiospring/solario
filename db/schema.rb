3073455efee1894699e261359ba57eb4b7b4ec5c
studiospring <sean@studiospring.com.au>
2013-09-24 14:18:54 +1000

    change db schema...
    add postcode_id to PvQuery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906055635) do
+ActiveRecord::Schema.define(version: 20130924000517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 20130906055635) do
   end
 
   create_table "pv_queries", force: true do |t|
-    t.integer  "postcode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "postcode_id"
   end
 
 end
