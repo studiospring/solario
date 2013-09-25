08bc016177ce828256293176e312d1e10cc2c980
studiospring <sean@studiospring.com.au>
2013-09-25 11:44:34 +1000

    tests fail but code works

--- app/controllers/pv_queries_controller.rb
+++ app/controllers/pv_queries_controller.rb
@@ -41,7 +41,7 @@ class PvQueriesController < ApplicationController
     @pv_query = PvQuery.find(params[:id])
   
     #TODO: bug
-    #postcode_id = Postcode.select('id').where("pcode = ?", @pv_query.postcode).first
+    @postcode_id = Postcode.select('id').where("pcode = ?", @pv_query.postcode_id).first.id
     #annual_dni = Irradiance.select('direct').where('postcode_id = ?', postcode_id).first.direct
     #@panels = Hash.new
     #@pv_query.panels.each do |panel|
