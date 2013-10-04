7946a5c7a0db3d21ed6ee3ff6e246ae33c0f8ee6
studiospring <sean@studiospring.com.au>
2013-10-04 16:32:35 +1000

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-    
-                          #length: {},
-                        #numericality: {},
-                        #inclusion: {}
+         :recoverable, :rememberable,  :validatable #, :trackable,
+
+  validates :username,  presence: true,
+                        uniqueness: true
+  validates :email, presence: true
+  validates :admin, inclusion: { in: [true, false] }
 
 end
