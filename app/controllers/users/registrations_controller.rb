52a1742ccc100f973ab31ae9d2a998dc611d5f30
studiospring <sean@studiospring.com.au>
2013-10-05 14:56:37 +1000

    signup saves user as admin

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,29 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  def create# <<<
+    @user = User.new(user_params)
+    @user.admin = true
+    if @user.save
+      flash[:success] = 'New user created'
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end# >>>
+  def edit# <<<
+    @user = User.find(params[:id])
+  end# >>>
+  def update# <<<
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'User updated'
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end# >>>
+  private
+    def user_params# <<<
+      #enter mass assignable fields here
+      params.require(:user).permit(:username, :email)
+    end # >>>
 end
