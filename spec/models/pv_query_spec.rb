278481a850c4807634f7630bb0e5b8594513f0ee
studiospring <sean@studiospring.com.au>
2013-10-15 21:01:28 +1100

    avg_output_pa fails, give up

--- spec/models/pv_query_spec.rb
+++ spec/models/pv_query_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe PvQuery do
+  #prevent tests from failing because of postcode_to_postcode_id callback
+  PvQuery.skip_callback(:validation, :before, :postcode_to_postcode_id)
   let(:postcode) { FactoryGirl.create(:postcode) }
-  before { @pv_query = postcode.pv_queries.build() }
+  before do
+    @pv_query = postcode.pv_queries.build()
+  end
+  let!(:irradiance) { FactoryGirl.create(:irradiance, postcode_id: @pv_query.postcode_id) }
+
   subject { @pv_query }
 
   it { should respond_to(:postcode_id) }
@@ -12,15 +18,15 @@ describe PvQuery do
 
   it { should be_valid }
 
-  describe 'when postcode_id is not present' do
+  describe 'when postcode_id is not present' do# <<<
     before { @pv_query.postcode_id = nil }
     it { should_not be_valid }
   end
   describe 'when postcode_id is not a number' do
     before { @pv_query.postcode_id = 'abcd' }
     it { should_not be_valid }
-  end
-  describe 'panel association' do
+  end# >>>
+  describe 'panel association' do# <<<
     before do
       @pv_query.save
     end
@@ -34,5 +40,11 @@ describe PvQuery do
         expect(Panel.where(id: panel.id)).to be_empty
       end
     end
-  end
+  end# >>>
+  describe 'avg_output_pa' do# <<<
+    it "should return totals of dni values for entire pv_query" do
+      #broken
+      @pv_query.avg_output_pa.should == '1 2'
+    end
+  end# >>>
 end
