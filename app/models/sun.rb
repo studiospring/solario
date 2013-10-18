6b59e81644913f3e7a4042d855082737dc07a8c4
studiospring <sean@studiospring.com.au>
2013-10-18 16:09:02 +1100

    delete unused sun methods

--- app/models/sun.rb
+++ app/models/sun.rb
@@ -9,30 +9,9 @@ class Sun
     @day = day
   end
 
-  #enter latitude in degrees
-  #return hourly elevation in radians for every day of year
-  #{ day1: [elev1, elev2...], day2: [elev1, elev2..]...}
-  def self.annual_elevation(latitude)# <<<
-    annual_elev = Hash.new
-    (1..365).each do |day|
-      annual_elev[day] = self.daily_elevation(day, latitude)
-    end
-    return annual_elev
-  end# >>>
-  #return hash of annual direct normal irradiance (KWh/sqm) for specific latitude
-  #table (or file) columns will be: latitude longitude dni(KWh/sqm)
-  #where dni will be a string to be converted in to a hash
-  #Array of dni values will be per UT hour
-  #0 values must be included so that time can be calculated from position in
-  #  array
-  #see ActiveRecord Serialize
-  #{ day1: [val1, val2...], day2: ... }
-  def annual_dni# <<<
-    #query database
-  end# >>>
   #return hash: vector[:x], [:y], [:z]
   #insert 24hr hourly time: 9, 12, 13 ...
-  #and 13.5 is returned for 13:30pm
+  #eg 13.5 is 13:30pm
   def vector(local_hour)# <<<
     lst = self.to_lst(local_hour)
     hra = self.hra(lst)
@@ -49,22 +28,6 @@ class Sun
     #return declination in radians
     def declination# <<<
       declination = Math.asin(0.3979486313076103 * Math.sin(0.017214206 * (self.day - 81))).abs
-    end# >>>
-    #enter latitude in degrees
-    #return hourly elevation in radians for one day
-    #[elev1, elev2 ...] 
-    def self.daily_elevation(day, latitude)# <<<
-      #hour angle, aka azimuth. 0 is midday at local solar time
-      hra = [ 0, 15, 30, 45, 60, 75, 90, 105, 120, 135 ]
-      declination = self.declination(day)
-      latitude = latitude.to_rad
-      pm_elev = Array.new
-      hra.each do |angle|
-        elevation = Math.asin(Math.sin(declination)*Math.sin(latitude) + Math.cos(declination)*Math.cos(latitude)*Math.cos(angle.to_rad))
-        elevation >= 0 ? pm_elev << elevation : break
-      end
-      daily_elev = Array.new
-      daily_elev = pm_elev.drop(1).reverse + pm_elev
     end# >>>
     #input hra in degrees bc degs is easier to understand
     #return elevation of sun in radians
--- spec/models/sun_spec.rb
+++ spec/models/sun_spec.rb
@@ -20,18 +20,6 @@ describe Sun do
       @sun.declination.should be_within(0.001).of(0.38333384764823003) 
     end
   end
-  #method no longer used
-  #describe 'daily_elevation method' do
-    #it "should return array of elevations" do
-       #Sun.daily_elevation(1, 33).should == [0.012858311423347368, 0.21432380448642885, 0.4252137951043474, 0.6419099839801242, 0.8612064961386582, 1.078663171526293, 1.2815459750954357, 1.396022715843284, 1.2815459750954357, 1.078663171526293, 0.8612064961386582, 0.6419099839801242, 0.4252137951043474, 0.21432380448642885, 0.012858311423347368]
-    #end
-  #end
-  #method no longer used
-  #describe 'annual_elevation method' do
-    #it "should include array of daily elevations within hash" do
-      #Sun.annual_elevation(33)[1].should == [0.012858311423347368, 0.21432380448642885, 0.4252137951043474, 0.6419099839801242, 0.8612064961386582, 1.078663171526293, 1.2815459750954357, 1.396022715843284, 1.2815459750954357, 1.078663171526293, 0.8612064961386582, 0.6419099839801242, 0.4252137951043474, 0.21432380448642885, 0.012858311423347368]
-    #end
-  #end
   describe 'vector instance method' do# <<<
     it "should return correct value for @sun.vector[:x]" do
       @sun.vector(9)[:x].should be_within(0.001).of(0.48662785778917966)
