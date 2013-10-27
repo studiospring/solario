e290c42a5c67960d54773fe942bdbf968647ef16
studiospring <sean@studiospring.com.au>
2013-10-28 10:32:20 +1100

    fix to_lst test

--- spec/models/solar_time_spec.rb
+++ spec/models/solar_time_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe SolarTime do
   let(:sun) { Sun.new(:latitude, :longitude, :day) }
   #let(:sun) { FactoryGirl.create(:sun) }
-  before { @sun = Sun.new(-20, 135, 10) }
+  before { @sun = Sun.new(-20, 135, 'WA', 10) }
   subject { @sun }
 
     describe 'to_lst' do# <<<
       before { @sun.to_lst(14) }
       it "should convert local time to local solar time" do
-        @sun.to_lst(14).should be_within(0.01).of(12.88)
+        @sun.to_lst(14).should be_within(0.01).of(14.875)
       end
     end# >>>
     describe 'hra' do
