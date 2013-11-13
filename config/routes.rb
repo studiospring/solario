4f637db2ad70ee7b81bf161268a60bf39e0b4b03
studiospring <sean@studiospring.com.au>
2013-11-13 12:49:52 +1100

    add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Solar::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root            to: 'pv_queries#new'
   get '/about',   to: 'static_pages#about'
+  get '/contact',   to: 'static_pages#contact'
   #get '/terms',   to: 'static_pages#terms'
 
   # Example of regular route:
