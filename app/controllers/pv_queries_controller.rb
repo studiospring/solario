9a8f292fd4f1127432c7665cc91faad056170589
studiospring <sean@studiospring.com.au>
2014-01-07 10:18:57 +1100

    retrieve pvoutput data

--- app/controllers/pv_queries_controller.rb
+++ app/controllers/pv_queries_controller.rb
@@ -1,4 +1,5 @@
 class PvQueriesController < ApplicationController
+  require 'net/http' #for api call to pvoutput.org
   #before_filter :authenticate_user!, except: [:new, :results]
   before_filter :require_admin, except: [:new, :create]
 
@@ -17,8 +18,28 @@ class PvQueriesController < ApplicationController
     if @pv_query.save
       @output_pa = @pv_query.avg_output_pa.join(' ') #convert from array to string
       #@column_heights = @pv_query.column_heights
-      #@total_output_pa = @pv_query.total_output_pa
+      @total_output_pa = @pv_query.total_output_pa
       @query_params = @pv_query.panels
+
+      uri = URI('http://pvoutput.org/service/r2/search.jsp')
+      req = Net::HTTP::Post.new(uri)
+      req['X-Pvoutput-Apikey'] = 'a0ac0021b1351c9658e4ff80c2bc5944405af134'
+      req['X-Pvoutput-SystemId'] = '26011'
+      params = { 'q'        => '2031',
+                 'country'  => 'Australia' }
+      req.set_form_data(params)
+
+      @search_results = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req)
+      end
+
+      #Net::HTTP.start(uri.host, uri.port) do |http|
+        #request = Net::HTTP::Get.new uri
+
+        #response = http.request request # Net::HTTPResponse object
+      #end
+      #@search_results = Net::HTTP.post_form(URI.parse('http://pvoutput.org/service/r2/search.jsp'), params)
+      #puts x.body
 
       respond_with({output_pa: @output_pa}, location: new_pv_query_url)
     else
