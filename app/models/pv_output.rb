02d2a700e593a110cd4051cd3e137a580301ed09
studiospring <sean@studiospring.com.au>
2014-01-08 16:17:00 +1100

    begin pvoutput.get_output

--- app/models/pv_output.rb
+++ app/models/pv_output.rb
@@ -19,33 +19,47 @@ class PvOutput
   end# >>>
   #send GET request to get info about system that contributes to pvoutput
   #returns hash of system info
+  #TODO: query by system id after donating
   def self.get_system# <<<
     response = self.request('getsystem')
-    keys = [ 'system_watts', 'panel_count', 'panel_watts', 'bearing', 'tilt', 'shade', 'sec_panel_count', 'sec_panel_watts', 'sec_bearing', 'sec_tilt' ]
+    keys = [ 'system_watts', 'panel_count', 'panel_watts', 'bearing', 'tilt', 'shade', 'install_date', 'sec_panel_count', 'sec_panel_watts', 'sec_bearing', 'sec_tilt' ]
     results_array = response.split(/,/)
-    results_array.values_at(3, 4, 9, 10, 11, 16, 17, 18, 19)
+    results_array.values_at(3, 4, 9, 10, 11, 12, 16, 17, 18, 19)
     #merges keys and system info to hash
     results = Hash[keys.zip results_array]
     return results
   end# >>>
-  #make request to pvoutput api and return response
-  def self.request(uri, params = {})# <<<
-    uri = URI.parse('http://pvoutput.org/service/r2/' + uri + '.jsp')
-    auth_params = { 'key' => Rails.application.secrets.pv_output_api_key,
-                    'sid' => Rails.application.secrets.pv_output_system_id }
-    #merge params params and add to uri
-    uri.query = URI.encode_www_form( auth_params.merge(params) )
-    begin
-      response = uri.read
-    rescue
-      return '' #fails silently
-    else
-      case response.status[0][0]
-        when '2', '3' 
-        return response
-      else
-        return '' #fails silently
-      end
+  #return array of hashes of daily output of system
+  #use to compare with theoretical daily output
+  #TODO: untested, unfinished. Query by system id after donating
+  def get_output(query_params = {})# <<<
+    response = self.request('getoutput', query_params)
+    keys = [ 'date', 'output' ] #output is in watt hours
+    results = Array.new
+    response.split(/;/).each do |daily_output|
+      results << Hash[keys.zip daily_output.values_at(0, 1)]
     end
+    return results
   end# >>>
+  private
+    #make GET request to pvoutput api and return response
+    def self.request(uri, params = {})# <<<
+      uri = URI.parse('http://pvoutput.org/service/r2/' + uri + '.jsp')
+      auth_params = { 'key' => Rails.application.secrets.pv_output_api_key,
+                      'sid' => Rails.application.secrets.pv_output_system_id }
+      #merge params params and add to uri
+      uri.query = URI.encode_www_form( auth_params.merge(params) )
+      begin
+        response = uri.read
+      rescue
+        return '' #fails silently
+      else
+        case response.status[0][0]
+          when '2', '3' 
+          return response
+        else
+          return '' #fails silently
+        end
+      end
+    end# >>>
 end
