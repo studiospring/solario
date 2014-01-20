0ee3e08eff1b112b28258348f1ca27d231a59259
studiospring <sean@studiospring.com.au>
2014-01-21 10:49:56 +1100

    js validation test passes...
    after failing correctly

--- spec/features/pv_query_pages_spec.rb
+++ spec/features/pv_query_pages_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "PV Query" do
   let(:base_title) { "Solario" }
-  let(:postcode) { FactoryGirl.create(:postcode) }
+  #let(:postcode) { FactoryGirl.create(:postcode) }
   #necessary because pv_queries_controller calls irradiance method
-  let!(:irradiance) { FactoryGirl.create(:irradiance, postcode_id: postcode.id) }
-  let!(:pv_query) { FactoryGirl.create(:pv_query, postcode_id: postcode.id) }
+  #let!(:irradiance) { FactoryGirl.create(:irradiance, postcode_id: postcode.id) }
+  #let!(:pv_query) { FactoryGirl.create(:pv_query, postcode_id: postcode.id) }
   subject { page }
 
   describe 'new page', js: true do# <<<
@@ -19,21 +19,14 @@ describe "PV Query" do
         fill_in "Bearing", with: 15
         fill_in "Tilt", with: 15
       end
-
-      #it "should create a new pv_query" do
-        #expect { click_button submit }.to change(PvQuery, :count).by(1)
-      #end
-      #it "should create a new panel" do
-        #expect { click_button submit }.to change(Panel, :count).by(1)
-      #end
-
-      #it { should have_selector("div.output_pa") }
-      #it { should have_link 'Logout', href: destroy_user_session_path }
-
-      #describe 'and logout' do
-        #before { click_link "Logout" }
-        #it { should have_link 'Login' }
-      #end
+    end
+    describe 'with invalid inputs' do
+      before do
+        fill_in "Postcode", with: 123
+        fill_in "Bearing", with: 15
+      end
+      it { should have_selector("label.alert-warning",
+                                text: "Please enter at least 4 characters.") }
     end
     
   end# >>>
@@ -43,7 +36,7 @@ describe "PV Query" do
     before do
       visit new_pv_query_path
     end
-    describe 'with valid inputs', js: true do
+    describe 'with valid inputs'do
       before do
         fill_in "Postcode", with: 1234
         fill_in "Bearing", with: 15
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+Capybara.default_wait_time = 5
 WebMock.disable_net_connect!(allow_localhost: true)
 
 Spork.prefork do
