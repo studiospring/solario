4b828900c49edf156789e3ddc602f6f51e880355
studiospring <sean@studiospring.com.au>
2014-01-21 16:14:57 +1100

    refactor js tests

--- spec/features/pv_query_pages_spec.rb
+++ spec/features/pv_query_pages_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe "PV Query" do
   let(:base_title) { "Solario" }
-  #let(:postcode) { FactoryGirl.create(:postcode) }
+  let(:postcode) { FactoryGirl.create(:postcode) }
   #necessary because pv_queries_controller calls irradiance method
-  #let!(:irradiance) { FactoryGirl.create(:irradiance, postcode_id: postcode.id) }
-  #let!(:pv_query) { FactoryGirl.create(:pv_query, postcode_id: postcode.id) }
+  let!(:irradiance) { FactoryGirl.create(:irradiance, postcode_id: postcode.id) }
+  let!(:pv_query) { FactoryGirl.create(:pv_query, postcode_id: postcode.id) }
   subject { page }
+  let(:heading) { 'Average annual panel output' }
+  let(:submit) { 'view output' }
 
+  before do
+    visit new_pv_query_path
+  end
   describe 'new page', js: true do# <<<
-    let(:heading) { 'Average annual panel output' }
-    let(:submit) { 'view output' }
-    before do
-      visit new_pv_query_path
-    end
     describe 'with javascript enabled' do
       it { should have_selector("#enable_js", visible: false) }
     end
@@ -22,9 +22,12 @@ describe "PV Query" do
         fill_in "Postcode", with: 1234
         fill_in "Bearing", with: 15
         fill_in "Tilt", with: 15
-        fill_in "Area", with: '15'
-        click_button submit
+        fill_in "Area", with: 15
+        #click_button submit
       end
+      #it "should create a new pv_query" do
+        #expect { click_button submit }.to change(PvQuery, :count).by(1)
+      #end
 
     end
     describe "after clicking 'add panel'" do
@@ -59,35 +62,32 @@ describe "PV Query" do
     
   end# >>>
   describe 'results page, js: true' do# <<<
-    let(:heading) { 'Average annual panel output' }
-    let(:submit) { 'view output' }
     before do
-      visit new_pv_query_path
+      fill_in "Postcode", with: 1234
+      fill_in "Bearing", with: 15
+      fill_in "Tilt", with: 15
+      fill_in "Area", with: 15
+      click_button submit
     end
-    describe 'with valid inputs'do
-      before do
-        fill_in "Postcode", with: 1234
-        fill_in "Bearing", with: 15
-        fill_in "Tilt", with: 15
-        fill_in "Area", with: 15
-        click_button submit
-      end
-      #it { should have_selector('h2', text: heading) }
+    #visit new_pv_query_path
+    it { should_not have_selector('legend') }
+    #print page.html
+    it { should have_css('input.test') }
+    #it { should have_selector('h2', text: heading) }
 
-      #it "should create a new pv_query" do
-        #expect { click_button submit }.to change(PvQuery, :count).by(1)
-      #end
-      #it "should create a new panel" do
-        #expect { click_button submit }.to change(Panel, :count).by(1)
-      #end
+    #it "should create a new pv_query" do
+      #expect { click_button submit }.to change(PvQuery, :count).by(1)
+    #end
+    #it "should create a new panel" do
+      #expect { click_button submit }.to change(Panel, :count).by(1)
+    #end
 
-     #it { should have_selector("div.output_pa") }
-      #it { should have_link 'Logout', href: destroy_user_session_path }
+   #it { should have_selector("div.output_pa") }
+    #it { should have_link 'Logout', href: destroy_user_session_path }
 
-      #describe 'and logout' do
-        #before { click_link "Logout" }
-        #it { should have_link 'Login' }
-      #end
-    end
+    #describe 'and logout' do
+      #before { click_link "Logout" }
+      #it { should have_link 'Login' }
+    #end
   end# >>>
 end
