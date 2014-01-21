b329d6ed3861e9e27c925a17bd3e33f94affc46a
studiospring <sean@studiospring.com.au>
2014-01-21 12:04:49 +1100

    test add/remove panel buttons

--- spec/features/pv_query_pages_spec.rb
+++ spec/features/pv_query_pages_spec.rb
@@ -9,6 +9,7 @@ describe "PV Query" do
   subject { page }
 
   describe 'new page', js: true do# <<<
+    let(:heading) { 'Average annual panel output' }
     let(:submit) { 'view output' }
     before do
       visit new_pv_query_path
@@ -18,6 +19,15 @@ describe "PV Query" do
         fill_in "Postcode", with: 1234
         fill_in "Bearing", with: 15
         fill_in "Tilt", with: 15
+      end
+    end
+    describe "after clicking 'add panel'" do
+      before { click_link 'add panel' }
+      it { should have_selector("input#pv_query_panels_attributes_1_bearing") }
+
+      describe "and clicking 'remove panel'" do
+        before { click_link 'remove panel' }
+        it { should_not have_selector("input#pv_query_panels_attributes_1_bearing") }
       end
     end
     describe 'with invalid inputs' do
@@ -37,6 +47,8 @@ describe "PV Query" do
                                 text: "This field is required.") }
       it { should have_selector("label.alert-warning",
                                 text: "This field is required.") }
+
+      it { should_not have_selector('h2', text: heading) }
     end
     
   end# >>>
@@ -54,7 +66,7 @@ describe "PV Query" do
         fill_in "Area", with: 15
         click_button submit
       end
-      it { should have_selector('h2', text: heading) }
+      #it { should have_selector('h2', text: heading) }
 
       #it "should create a new pv_query" do
         #expect { click_button submit }.to change(PvQuery, :count).by(1)
@@ -63,7 +75,7 @@ describe "PV Query" do
         #expect { click_button submit }.to change(Panel, :count).by(1)
       #end
 
-      it { should have_selector("div.output_pa") }
+     #it { should have_selector("div.output_pa") }
       #it { should have_link 'Logout', href: destroy_user_session_path }
 
       #describe 'and logout' do
