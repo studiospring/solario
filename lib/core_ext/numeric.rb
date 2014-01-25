63040f13c599e4a14675472afd552ba3d07c1f08
studiospring <sean@studiospring.com.au>
2014-01-25 14:00:47 +1100

    add deg_to_north

--- lib/core_ext/numeric.rb
+++ lib/core_ext/numeric.rb
@@ -2,7 +2,11 @@
 #require 'core_ext/numeric'
 class Numeric
   #convert degrees to radians. eg 45.degrees => 1.0
-  def to_rad
+  def to_rad# <<<
     self * Math::PI / 180 
-  end
+  end# >>>
+  #return difference in degrees from north
+  def deg_to_north# <<<
+    deg = 180 - ((360 - self).abs - 180).abs
+  end# >>>
 end
