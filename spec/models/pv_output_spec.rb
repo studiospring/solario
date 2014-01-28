5204cafe9d1578ccc88c35fb61cb570ae18c74ae
studiospring <sean@studiospring.com.au>
2014-01-28 11:09:41 +1100

    cannot get pvoutput.search spec to pass

--- spec/models/pv_output_spec.rb
+++ spec/models/pv_output_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe PvOutput do
+  #let(:postcode) { FactoryGirl.create(:postcode) }
 
   #stubs are defined in spec_helper.rb
   describe 'search' do# <<<
-    before { @systems_array = PvOutput.search('2031') }
     it 'returns array of systems from pvoutput.org' do
-      @systems_array[0].should == {"name"=>" Solar 4 US", "size"=>"9360", "postcode"=>"4280", "orientation"=>"NW", "outputs"=>"81", "last_output"=>"2 days ago", "system_id"=>"249", "panel"=>"Solarfun", "inverter"=>"Aurora", "distance"=>"NaN", "latitude"=>"-27.831402", "longitude"=>"153.028469"}
+      #does not work
+      #postcode = double('Postcode')
+      #postcode.stub(:update_urban?).and_return(true)
+      #expect(PvOutput.search('2031')[0]).to eq '2'
+      PvOutput.search('2031')[0].should == {"name"=>" Solar 4 US", "size"=>"9360", "postcode"=>"4280", "orientation"=>"NW", "outputs"=>"81", "last_output"=>"2 days ago", "system_id"=>"249", "panel"=>"Solarfun", "inverter"=>"Aurora", "distance"=>"NaN", "latitude"=>"-27.831402", "longitude"=>"153.028469"}
     end
   end# >>>
   describe 'get_system' do# <<<
