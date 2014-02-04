a972655c793a8de8c00dc13dff812d30b4bcf0ce
studiospring <sean@studiospring.com.au>
2014-02-04 11:06:37 +1100

    pvoutput.get_system spec passes

--- spec/models/pv_output_spec.rb
+++ spec/models/pv_output_spec.rb
@@ -17,10 +17,6 @@ describe PvOutput do
   end# >>>
   describe 'search' do# <<<
     it 'returns array of systems from pvoutput.org' do
-      #does not work
-      #postcode = double('Postcode')
-      #postcode.stub(:update_urban?).and_return(true)
-      #expect(PvOutput.search('2031')[0]).to eq '2'
       PvOutput.search('4280 +NW')[0].should == {"name"=>" Solar 4 US", "system_watts"=>"9360", "postcode"=>"4280", "orientation"=>"NW", "entries"=>"81", "last_entry"=>"2 days ago", "id"=>"249", "panel"=>"Solarfun", "inverter"=>"Aurora", "distance"=>"NaN", "latitude"=>"-27.831402", "longitude"=>"153.028469"}
     end
     it "should update postcode" do
@@ -29,7 +25,20 @@ describe PvOutput do
   end# >>>
   describe 'get_system' do# <<<
     it 'should return system data from pvoutput.org' do
-      PvOutput.get_system('100').should == "PVOutput Demo,2450,2199,14,175,Enertech,1,2000,CMS,N,NaN,No,20100101,-33.907725,151.026108,5;60.0,20.5,8.0,15.0,NaN,40.0;1,12,93;1"
+      PvOutput.get_system('100').should == 
+        { 'id' => '100',
+          'system_watts' => '2450',
+          'panel_count' => '14',
+          'panel_watts' => '175',
+          'orientation' => 'N',
+          'tilt' => 'NaN',
+          'shade' => 'No',
+          'install_date' => '20100101',
+          'sec_panel_count' => '10',
+          'sec_panel_watts' => '190',
+          'sec_bearing' => 'W',
+          'sec_tilt' => '30.5'
+        }
     end
   end# >>>
   describe 'get_output' do# <<<
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,27 +29,23 @@ Spork.prefork do
   RSpec.configure do |config|
     #replace requests to Pvoutput.org with stubs, using Webmock 
     config.before(:each) do
-      #stub_request(:get, "http://pvoutput.org/service/r2/search.jsp?country=Australia&key=&#{Rails.application.secrets.pvo_api_key}&q=4280%20%2BNW&sid=#{Rails.application.secrets.pvo_system_id}").
-        #to_return(
-          #:status => 200, 
-          #:body => " Solar 4 US,9360,4280,NW,81,2 days ago,249,Solarfun,Aurora,NaN,-27.831402,153.028469
-                    #Solar Chaos,1480,4870,NW,14,5 weeks ago,694,ET Solar ET-M572185,PCM Solar King 1500,NaN,-16.883938,145.746732
-                    #Solar Frontier 2.97KW 2768,2952,2768,W,72,Yesterday,387,Solar Frontier,Xantrex 2.8 AU,NaN,-33.737863,150.922732
-                    #solar powered muso,3600,5074,NW,146,5 days ago,151,Sunpower,Fronius,NaN,-34.878302,138.663553")
-
-      stub_request(:get, "http://pvoutput.org/service/r2/search.jsp?country=Australia&key=d4688e594b218f1746dec6cff0e34c2216dc675e&q=4280%20%2BNW&sid=26011").
-           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-           to_return(
-             :status => 200,
-             :body => " Solar 4 US,9360,4280,NW,81,2 days ago,249,Solarfun,Aurora,NaN,-27.831402,153.028469
-                    Solar Chaos,1480,4870,NW,14,5 weeks ago,694,ET Solar ET-M572185,PCM Solar King 1500,NaN,-16.883938,145.746732
-                    Solar Frontier 2.97KW 2768,2952,2768,W,72,Yesterday,387,Solar Frontier,Xantrex 2.8 AU,NaN,-33.737863,150.922732
-                    solar powered muso,3600,5074,NW,146,5 days ago,151,Sunpower,Fronius,NaN,-34.878302,138.663553")
+      stub_request(:get, "http://pvoutput.org/service/r2/search.jsp?country=Australia&key=#{Rails.application.secrets.pvo_api_key}&q=4280%20%2BNW&sid=#{Rails.application.secrets.pvo_system_id}").
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(
+          :status => 200,
+          :body => " Solar 4 US,9360,4280,NW,81,2 days ago,249,Solarfun,Aurora,NaN,-27.831402,153.028469
+                 Solar Chaos,1480,4870,NW,14,5 weeks ago,694,ET Solar ET-M572185,PCM Solar King 1500,NaN,-16.883938,145.746732
+                 Solar Frontier 2.97KW 2768,2952,2768,W,72,Yesterday,387,Solar Frontier,Xantrex 2.8 AU,NaN,-33.737863,150.922732
+                 solar powered muso,3600,5074,NW,146,5 days ago,151,Sunpower,Fronius,NaN,-34.878302,138.663553")
 
       stub_request(:get, "http://pvoutput.org/service/r2/getsystem.jsp?key=#{Rails.application.secrets.pvo_api_key}&sid=#{Rails.application.secrets.pvo_system_id}&sid1=100").
         to_return(
           :status => 200, 
-          :body => "PVOutput Demo,2450,2199,14,175,Enertech,1,2000,CMS,N,NaN,No,20100101,-33.907725,151.026108,5;60.0,20.5,8.0,15.0,NaN,40.0;1,12,93;1")
+          :body => "PVOutput Demo,2450,2199,14,175,Enertech,1,2000,CMS,N,NaN,No,20100101,-33.907725,151.026108,5;60.0,10,190,W,30.5,40.0;1,12,93;1")
+
+      stub_request(:get, "http://pvoutput.org/service/r2/getstatistic.jsp?date_from=20100901&date_to=20100930&key=#{Rails.application.secrets.pvo_api_key}&sid=#{Rails.application.secrets.pvo_api_key}&sid1=1000").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => "", :headers => {})
     end
 
     # ## Mock Framework
