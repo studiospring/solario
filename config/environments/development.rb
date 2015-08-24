5c496ba1a22499b9a5d2e792cb2fc7fa798b2586
Sean <lettersforsean@yahoo.co.jp>
2015-08-24 15:45:43 +1000

    Convert views to slim.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,6 @@ Solar::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  Slim::Engine.set_options pretty: true, sort_attrs: false
 end
