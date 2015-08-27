d38f6904de9ed4184f67576d0b687420ac5186e5
Sean <lettersforsean@yahoo.co.jp>
2015-08-27 17:08:26 +1000

    Get capybara and webmock set up in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -15,6 +19,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 20
+WebMock.disable_net_connect!(:allow_localhost => true)
 
 RSpec.configure do |config|
   # ## Mock Framework
