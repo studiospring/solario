8868a7870d665159001eade096b5028ba6bab7fb
Sean <lettersforsean@yahoo.co.jp>
2015-09-21 14:47:18 +1000

    PvoStub spec utility creates stub_request.

--- spec/models/pv_output_spec.rb
+++ spec/models/pv_output_spec.rb
@@ -6,10 +6,15 @@ describe PvOutput do
   let(:system_id) { 'my_system_id' }
 
   let(:search_body) do
-    "Solar 4 US,9360,4280,NW,81,2 days ago,249,Solarfun,Aurora,NaN,-27.831402,153.028469
-    Solar Chaos,1480,4870,NW,14,5 weeks ago,694,ET Solar ET-M572185,PCM Solar King 1500,NaN,-16.883938,145.746732
-    Solar Frontier 2.97KW 2768,2952,2768,W,72,Yesterday,387,Solar Frontier,Xantrex 2.8 AU,NaN,-33.737863,150.922732
-    solar powered muso,3600,5074,NW,146,5 days ago,151,Sunpower,Fronius,NaN,-34.878302,138.663553"
+    "Solar 4 US,9360,4280,NW,81,2 days ago,249,
+    Solarfun,Aurora,NaN,-27.831402,153.028469
+    Solar Chaos,1480,4870,NW,14,5 weeks ago,694,
+    ET Solar ET-M572185,
+    PCM Solar King 1500,NaN,-16.883938,145.746732
+    Solar Frontier 2.97KW 2768,2952,2768,W,72,Yesterday,387,
+    Solar Frontier,Xantrex 2.8 AU,NaN,-33.737863,150.922732
+    solar powered muso,3600,5074,NW,146,5 days ago,151,
+    Sunpower,Fronius,NaN,-34.878302,138.663553"
   end
 
   let(:postcode) { FactoryGirl.create(:postcode) }
@@ -20,10 +25,8 @@ describe PvOutput do
 
   describe 'find_similar_system' do
     context 'when similar system is found' do
-      before :each do
-        stub_request(:get, "#{base_uri}search.jsp?country=Australia&key=#{api_key}&q=4870%20%2BNW&sid=#{system_id}")
-          .to_return(:status => 200, :body => search_body)
-      end
+      let(:query) { {:country => 'Australia', :q => '4870 %2BNW'} }
+      let!(:stub) { PvoStub.new('search', "", query) }
 
       it "should return hash if similar system is found" do
         expect(PvOutput.find_similar_system("4870 +NW")).to eq({})
@@ -31,10 +34,8 @@ describe PvOutput do
     end
 
     context 'when similar system is not found' do
-      before :each do
-        stub_request(:get, "#{base_uri}search.jsp?country=Australia&key=#{api_key}&q=1234%20%2BS&sid=#{system_id}")
-          .to_return(:status => 200, :body => "")
-      end
+      let(:query) { {:country => 'Australia', :q => '1234 %2BS'} }
+      let!(:stub) { PvoStub.new('search', "", query) }
 
       it "should return empty hash if system not found" do
         expect(PvOutput.find_similar_system("1234 +S")).to eq({})
@@ -59,10 +60,8 @@ describe PvOutput do
       }
     end
 
-    before do
-      stub_request(:get, "#{base_uri}search.jsp?country=Australia&key=#{api_key}&q=4280%20%2BNW&sid=#{system_id}")
-         .to_return(:status => 200, :body => search_body)
-    end
+    let(:query) { {:country => 'Australia', :q => '4280 %2BNW'} }
+    let!(:stub) { PvoStub.new('search', "", query) }
 
     it 'returns array of systems from pvoutput.org' do
       expect(PvOutput.search('4280 +NW')[0]).to eq(pvo_systems)
@@ -74,6 +73,10 @@ describe PvOutput do
   end
 
   describe 'get_system' do
+    # rubocop:disable Metrics/LineLength
+    let(:system_body) { "PVOutput Demo,2450,2199,14,175,Enertech,1,2000,CMS,N,NaN,No,20100101,-33.907725,151.026108,5;60.0,10,190,W,30.5,40.0;1,12,93;1" }
+    # rubocop:enable Metrics/LineLength
+
     let(:system_data) do
       { 'id' => '100',
         'system_watts' => '2450',
@@ -87,15 +90,11 @@ describe PvOutput do
         'sec_panel_watts' => '190',
         'sec_bearing' => 'W',
         'sec_tilt' => '30.5'
-                    }
+      }
     end
 
-    before do
-      stub_request(:get, "#{base_uri}getsystem.jsp?key=#{api_key}&sid=#{system_id}&sid1=100")
-        .to_return(
-          :status => 200,
-          :body => "PVOutput Demo,2450,2199,14,175,Enertech,1,2000,CMS,N,NaN,No,20100101,-33.907725,151.026108,5;60.0,10,190,W,30.5,40.0;1,12,93;1")
-    end
+    let(:query) { {:sid1 => '100'} }
+    let!(:stub) { PvoStub.new('getsystem', system_body, query) }
 
     it 'should return system data from pvoutput.org' do
       expect(PvOutput.get_system('100')).to eq(system_data)
@@ -109,10 +108,19 @@ describe PvOutput do
   end
 
   describe 'get_statistic' do
-    let(:query_params) do
+    let(:query) do
       { :sid1 => '1000',
-        :date_from => '20100901',
-        :date_to => '20100927'
+        :df => '20100901',
+        :dt => '20100927'
+      }
+    end
+
+    let(:stat_body) { "246800,246800,8226,2000,11400,3.358,27,20100901,20100927,4.653,20100916" }
+
+    let(:query) do
+      { :sid1 => '1000',
+        :df => '20100901',
+        :dt => '20100927'
       }
     end
 
@@ -124,15 +132,10 @@ describe PvOutput do
       }
     end
 
-    before do
-      stub_request(:get, "#{base_uri}getstatistic.jsp?date_from=20100901&date_to=20100927&key=#{api_key}&sid=#{system_id}&sid1=1000")
-        .to_return(
-          :status => 200,
-          :body => "246800,246800,8226,2000,11400,3.358,27,20100901,20100927,4.653,20100916")
-    end
+    let!(:stub) { PvoStub.new('getstatistic', stat_body, query) }
 
     it 'returns hash of system data from pvoutput.org' do
-      expect(PvOutput.get_statistic(query_params)).to eq(system_stats)
+      expect(PvOutput.get_statistic(query)).to eq(system_stats)
     end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,47 @@
 include ApplicationHelper
+require 'webmock'
+include WebMock::API
+
+# Webmock does not parse non-alphanum chars (eg +) correctly,
+#   nor does this class (except for spaces).
+#   Therefore, you need to pass in params properly encoded.
+# @arg [String], [String], [Hash].
+# @return [Webmock::API.stub_request].
+class PvoStub
+  attr_reader :service, :query, :body
+
+  def initialize(service, body, **params)
+    @service = service
+    @query = build_query(params)
+    @body = body
+    stub
+  end
+
+  # @return [Webmock::API.stub_request].
+  def stub
+    stub_request(:get, construct_uri)
+      .with(:headers => headers)
+      .to_return(:status => 200, :body => @body, :headers => {})
+  end
+
+  # @return [String] (should be addressable).
+  def construct_uri
+    "http://http//www.pvoutput.org:80/service/r2/#{@service}.jsp/#{@query}"
+  end
+
+  # @arg [Hash]
+  # @return [String] query part of uri.
+  def build_query(**params)
+    ary = params.each_with_object([]) { |(k, v), s| s << "#{k}=#{v}" }
+    ary.join('&').gsub(/\s/, '%20').prepend('?') unless ary.empty?
+  end
+
+  def headers
+    {'Accept' => '*/*',
+     'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+     'User-Agent' => 'Ruby',
+     'X-Pvoutput-Apikey' => Rails.application.secrets.pvo_api_key,
+     'X-Pvoutput-Systemid' => Rails.application.secrets.pvo_system_id
+    }
+  end
+end
